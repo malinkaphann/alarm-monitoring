e5325c1ed40c5d4a2029966d915d301d823af52d
malinka <malinkaphann@gmail.com>
2021-12-08 09:05:36 +0700

    update readme and refactor alarm.js a little bit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Alarm Monitoring App
+
+This is an Alarm Monitoring Application, implemented using [Nextjs Framework](https://nextjs.org)
+
+## DEV vs BUILD vs START
+
+Find more details about these command in here.
+
+[Nextjs Commands](https://nextjs.org/docs/api-reference/cli)
+
+## Install dependencies
+
+```
+yarn
+```
+
+## Run by using development's variables
+
+```
+yarn dev
+```
+
+## Run by using production's variables
+
+```
+yarn dev:prod
+```
+
+## Build by using development's variables
+
+```
+yarn build
+
+```
+
+## Build by using production's variables
+
+```
+yarn build:prod
+
+```
+
+## Start
+
+This depends on the **build** command. So the **start** command will run using those variables in the **build** command.
+
+
+```
+yarn start
+
+```
--- src/pages/api/alarm.js
+++ src/pages/api/alarm.js
@@ -1,138 +1,141 @@
-import { ALARMS } from './AlarmData';
-import ConfigCatWrapper from './ConfigCatWrapper';
-import LoggerWrapper from './LoggerWrapper';
-import SocketIOWrapper from './SocketIOWrapper';
+import { ALARMS } from "./AlarmData";
+import ConfigCatWrapper from "./ConfigCatWrapper";
+import LoggerWrapper from "./LoggerWrapper";
+import SocketIOWrapper from "./SocketIOWrapper";
 
 // to avoid error
 process.setMaxListeners(0);
 
-const IS_MONITORING_ENABLED_FLAG = 
-process.env.NEXT_PUBLIC_IS_MONITORING_ENABLED;
+const IS_MONITORING_ENABLED_FLAG =
+  process.env.NEXT_PUBLIC_IS_MONITORING_ENABLED;
 
 const statusChannel = IS_MONITORING_ENABLED_FLAG;
-const alarmChannel = 'alarms';
+const alarmChannel = "alarms";
 
 // broadcast alarms
 const sendAlarms = (isMonitoringEnabled, io) => {
-    if (isMonitoringEnabled) {
-        io.emit(alarmChannel, {
-            status: 2000,
-            data: ALARMS
-        });
-    } else {
-        io.emit(alarmChannel, {
-            status: 3000,
-            message: 'alarm monitoring is not available at this moment'
-        });
-    }
-}
+  if (isMonitoringEnabled) {
+    io.emit(alarmChannel, {
+      status: 2000,
+      data: ALARMS,
+    });
+  } else {
+    io.emit(alarmChannel, {
+      status: 3000,
+      message: "alarm monitoring is not available at this moment",
+    });
+  }
+};
+
+// this is a callback which is called everytime
+// a config is changed at configcat.
+const configChangeHandler = async () => {
+  LoggerWrapper.info("config has changed");
+
+  const isMonitoringEnabled = await ConfigCatWrapper.getValueAsync(
+    IS_MONITORING_ENABLED_FLAG
+  );
+
+  LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
+                ${isMonitoringEnabled}`);
+
+  const io = SocketIOWrapper.getServer();
+  if (!io) {
+    throw new Error("socket server io is not yet created");
+  }
+
+  LoggerWrapper.info(`statusChannel = ${statusChannel} broadcasted message = 
+                ${isMonitoringEnabled}`);
+
+  io.emit(statusChannel, isMonitoringEnabled);
+  sendAlarms(isMonitoringEnabled, io);
+};
 
 // main entry
 const MyController = async (req, res) => {
+  // catch all exceptions
+  try {
+    // when this request handler is accessed for the very first time
+    if (!SocketIOWrapper.getServer()) {
+      LoggerWrapper.info(
+        "socket io server is not yet created; \
+                so create one now"
+      );
 
-    // catch all exceptions
-    try {
+      const io = SocketIOWrapper.createServer(res.socket.server);
 
-        // this is a callback which is called everytime 
-        // a config is changed at configcat.
-        const configChangeHandler = async () => {
-            LoggerWrapper.info('config has changed');
+      // this should never happens,
+      // but just in case.
+      if (!io) {
+        throw new Error(
+          "unexpected error, \
+                    socket io server can not be created"
+        );
+      }
 
-            const isMonitoringEnabled = await 
-            ConfigCatWrapper.getValueAsync(IS_MONITORING_ENABLED_FLAG);
-
-            LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
-                ${isMonitoringEnabled}`);
-
-            const io = SocketIOWrapper.getServer();
-            if (!io) {
-                throw new Error('socket server io is not yet created');
-            }
-
-            LoggerWrapper.info(`statusChannel = ${statusChannel} broadcasted message = 
-                ${isMonitoringEnabled}`);
-
-            io.emit(statusChannel, isMonitoringEnabled);
-            sendAlarms(isMonitoringEnabled, io);
+      // on client connection, get config value
+      io.on("connection", async (socket) => {
+        // this should never happens,
+        // but just in case.
+        if (!ConfigCatWrapper.getClient()) {
+          throw new Error(
+            "unexpected error, \
+                        config cat client is not yet created"
+          );
         }
 
-        // when this request handler is accessed for the very first time
-        if (!SocketIOWrapper.getServer()) {
+        const isMonitoringEnabled = await ConfigCatWrapper.getValueAsync(
+          IS_MONITORING_ENABLED_FLAG
+        );
 
-            LoggerWrapper.info('socket io server is not yet created; \
-                so create one now');
-
-            const io = SocketIOWrapper.createServer(res.socket.server);
-
-            // this should never happens, 
-            // but just in case.
-            if (!io) {
-                throw new Error('unexpected error, \
-                    socket io server can not be created');
-            }
-
-            // on client connection, get config value
-            io.on('connection', async (socket) => {
-
-                // this should never happens, 
-                // but just in case.
-                if (!ConfigCatWrapper.getClient()) {
-                    throw new Error('unexpected error, \
-                        config cat client is not yet created');
-                }
-
-                const isMonitoringEnabled = await 
-                    ConfigCatWrapper.getValueAsync(IS_MONITORING_ENABLED_FLAG);
-
-                LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
+        LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
                 ${isMonitoringEnabled}`);
 
-                socket.emit(statusChannel, isMonitoringEnabled)
-                sendAlarms(isMonitoringEnabled, socket);
-            });
+        socket.emit(statusChannel, isMonitoringEnabled);
+        sendAlarms(isMonitoringEnabled, socket);
+      });
 
-            LoggerWrapper.info('configcat client is not yet created; \
-                so create one now');
+      LoggerWrapper.info(
+        "configcat client is not yet created; \
+                so create one now"
+      );
 
-            const client = ConfigCatWrapper.createClient(configChangeHandler);
+      const client = ConfigCatWrapper.createClient(configChangeHandler);
 
-            // this should never hapens,
-            // but just in case
-            if (!client) {
-                throw new Error('can not create configcat client');
-            }
-        }
-
-        // to handle the GET request from client
-        if (req.method === 'GET') {
-
-            // socket io server is supposed to be already created
-            const io = SocketIOWrapper.getServer();
-            if (!io) {
-                throw new Error('socket io is not yet created');
-            }
-
-            LoggerWrapper.info('receive a GET request from client');
-
-            const isMonitoringEnabled = await 
-                ConfigCatWrapper.getValueAsync(IS_MONITORING_ENABLED_FLAG);
-
-            LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
-                ${isMonitoringEnabled}`);
-
-            io.emit(statusChannel, isMonitoringEnabled)
-            sendAlarms(isMonitoringEnabled, io);
-
-            res.end();
-        }
-        
-    } catch (error) {
-
-        // can not afford to flush the response everytime
-        // there is an exception, so handle it here in one place.
-        LoggerWrapper.error(error.message);
-        res.end();
+      // this should never hapens,
+      // but just in case
+      if (!client) {
+        throw new Error("can not create configcat client");
+      }
     }
-}
+
+    // to handle the GET request from client
+    if (req.method === "GET") {
+      // socket io server is supposed to be already created
+      const io = SocketIOWrapper.getServer();
+      if (!io) {
+        throw new Error("socket io is not yet created");
+      }
+
+      LoggerWrapper.info("receive a GET request from client");
+
+      const isMonitoringEnabled = await ConfigCatWrapper.getValueAsync(
+        IS_MONITORING_ENABLED_FLAG
+      );
+
+      LoggerWrapper.info(`FROM ConfigCat: ${IS_MONITORING_ENABLED_FLAG} = 
+                ${isMonitoringEnabled}`);
+
+      io.emit(statusChannel, isMonitoringEnabled);
+      sendAlarms(isMonitoringEnabled, io);
+    }
+  } catch (error) {
+    // just log the error
+    LoggerWrapper.error(error.message);
+  } finally {
+    // flush the response in one place
+    res.end();
+  }
+};
 
 export default MyController;
